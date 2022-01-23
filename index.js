24545b463e0f5ce2eaa336f115c1453d207d3663
ImNotCreepy <79331628+ImNotCreepy@users.noreply.github.com>
2022-01-24 01:18:47 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,222 @@
+// =============== Port ===============
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => res.send('Bot Is Working Well!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// ===============================================================
+const fs = require('fs');
+const discord = require('discord.js-self');
+const client = new discord.Client({ disableMentions: 'everyone' });
+
+const { Player } = require('discord-player');
+
+client.player = new Player(client);
+client.config = require('./config/bot');
+client.emotes = client.config.emojis;
+client.filters = client.config.filters;
+client.commands = new discord.Collection();
+
+//coded by ImNotCreepy
+
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
+
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+
+for (const file of events) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
+
+for (const file of player) {
+    console.log(`Loading discord-player event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
+
+//-------------------------
+const ytdl = require("discord-ytdl-core");
+const youtubeScraper = require("yt-search");
+const yt = require("ytdl-core");
+const { MessageEmbed } = require("discord.js-self");
+
+exports.run = async (client, message, args) => {
+  const channel = message.member.voice.channel;
+
+  const error = (err) => message.channel.send(err);
+  const send = (content) => message.channel.send(content);
+  const setqueue = (id, obj) => message.client.queue.set(id, obj);
+  const deletequeue = (id) => message.client.queue.delete(id);
+  var song;
+
+  if (!channel) return error("You must join a voice channel to play music!");
+
+  if (!channel.permissionsFor(message.client.user).has("CONNECT"))
+    return error("I don't have permission to join the voice channel");
+
+  if (!channel.permissionsFor(message.client.user).has("SPEAK"))
+    return error("I don't have permission to speak in the voice channel");
+
+  const query = args.join(" ");
+
+  if (!query) return error("You didn't provide a song name to play!");
+
+  if (query.includes("www.youtube.com")) {
+    try {
+      const ytdata = await await yt.getBasicInfo(query);
+      if (!ytdata) return error("No song found for the url provided");
+      song = {
+        name: Util.escapeMarkdown(ytdata.videoDetails.title),
+        thumbnail:
+          ytdata.player_response.videoDetails.thumbnail.thumbnails[0].url,
+        requested: message.author,
+        videoId: ytdata.videoDetails.videoId,
+        duration: forHumans(ytdata.videoDetails.lengthSeconds),
+        url: ytdata.videoDetails.video_url,
+        views: ytdata.videoDetails.viewCount,
+      };
+    } catch (e) {
+      console.log(e);
+      return error("Error occured, please check console");
+    }
+  } else {
+    try {
+      const fetched = await (await youtubeScraper(query)).videos;
+      if (fetched.length === 0 || !fetched)
+        return error("I couldn't find the song you requested!'");
+      const data = fetched[0];
+      song = {
+        thumbnail: data.image,
+        requested: message.author,
+        videoId: data.videoId,
+        duration: data.duration.toString(),
+        url: data.url,
+        views: data.views,
+      };
+    } catch (err) {
+      console.log(err);
+      return error("An error occured, Please check console");
+    }
+  }
+
+  var list = message.client.queue.get(message.guild.id);
+
+  if (list) {
+    list.queue.push(song);
+    return send(
+      new MessageEmbed()
+        .setAuthor(
+          "The song has been added to the queue",
+          "https://img.icons8.com/color/2x/cd--v3.gif"
+        )
+        .setColor("F93CCA")
+        .setThumbnail(song.thumbnail)
+        .addField("Song Name", song.name, false)
+        .addField("Views", song.views, false)
+        .addField("Duration", song.duration, false)
+        .addField("Requested By", song.requested.tag, false)
+        .setFooter("Positioned " + list.queue.length + " In the queue")
+    );
+  }
+
+  const structure = {
+    channel: message.channel,
+    vc: channel,
+    volume: 85,
+    playing: true,
+    queue: [],
+    connection: null,
+  };
+
+  setqueue(message.guild.id, structure);
+  structure.queue.push(song);
+
+  try {
+    const join = await channel.join();
+    structure.connection = join;
+    play(structure.queue[0]);
+  } catch (e) {
+    console.log(e);
+    deletequeue(message.guild.id);
+    return error("I couldn't join the voice channel, Please check console");
+  }
+
+  async function play(track) {
+    try {
+      const data = message.client.queue.get(message.guild.id);
+      if (!track) {
+        data.channel.send("Queue is empty, Leaving voice channel");
+        message.guild.me.voice.channel.leave();
+        return deletequeue(message.guild.id);
+      }
+      data.connection.on("disconnect", () => deletequeue(message.guild.id));
+      const source = await ytdl(track.url, {
+        filter: "audioonly",
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+        opusEncoded: true,
+      });
+      const player = data.connection
+        .play(source, { type: "opus" })
+        .on("finish", () => {
+          var removed = data.queue.shift();
+          if(data.loop == true){
+            data.queue.push(removed)
+          }
+          play(data.queue[0]);
+        });
+      player.setVolumeLogarithmic(data.volume / 100);
+      data.channel.send(
+        new MessageEmbed()
+          .setAuthor(
+            "Started Playing",
+            "https://img.icons8.com/color/2x/cd--v3.gif"
+          )
+          .setColor("9D5CFF")
+          .setThumbnail(track.thumbnail)
+          .addField("Song Name", track.name, false)
+          .addField("Views", track.views, false)
+          .addField("Duration", track.duration, false)
+          .addField("Requested By", track.requested, false)
+          .setFooter("Youtube Music Player")
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+
+const ownername = "" 
+const status = [
+  "dnd", 
+  "idle", 
+  "online"
+]
+
+// ===============================
+client.on('ready', () => {  
+    console.log(`Logged in as ${client.user.tag}!`);
+  setInterval(() => {
+    var stats = status[Math.floor(Math.random()*status.length)];
+	console.log(`Status Changed`);
+    client.user.setPresence({
+        status: stats,
+        }
+      )
+  }, 1000);
+
+    });
+
+client.login(client.config.discord.token);
